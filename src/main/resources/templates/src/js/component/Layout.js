75921f6d438553631611db23c4bc8c5fdabab70a
ggikko <ggikko@hotmail.com>
2016-03-04 13:03:30 +0900

    add state example

--- src/main/resources/templates/src/js/component/Layout.js
+++ src/main/resources/templates/src/js/component/Layout.js
@@ -6,10 +6,19 @@ import Header from './Header.js';
 import Footer from './Footer.js';
 
 class Layout extends React.Component{
+    constructor(){
+        super();
+        this.state = {value : 'first'};
+    }
+
     render(){
+        setTimeout(()=> {
+            this.setState({value : 'second'});
+        }, 2000)
         const name = "JiHong Park";
         return (
             <div>
+                {this.state.value}
                 <Header/>
                 <h1>content</h1>
                 <Footer/>
